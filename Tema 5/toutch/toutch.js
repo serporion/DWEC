45fdcb717e0d001634cc77e1a47cf1734610730d
serporion <serporionGit@hotmail.com>
2024-11-05 22:28:50 +0100

    Toutch v3d

--- Tema 5/toutch/toutch.js
+++ Tema 5/toutch/toutch.js
@@ -1,85 +1,85 @@
 
-///window.onload = () => {
+window.onload = () => {
 
-let pulsados = document.querySelector("section");
+    let pulsados = document.querySelector("section");
 
-let div3 = document.getElementById("3")
+    let div3 = document.getElementById("3")
 
-let input = document.getElementById("puntos");
+    let input = document.getElementById("puntos");
 
-//pulsados.addEventListener('click', (e) => console.log("se ha pulsado " + e.target.id ))
+    //pulsados.addEventListener('click', (e) => console.log("se ha pulsado " + e.target.id ))
 
-let puntosRecorridos = [];
-
-
-pulsados.addEventListener("touchstart", handleStart, false);
-pulsados.addEventListener("touchmove", handleMove, false);
-pulsados.addEventListener("touchend", handleEnd, false);
+    let puntosRecorridos = [];
 
 
+    pulsados.addEventListener("touchstart", handleStart, false);
+    pulsados.addEventListener("touchmove", handleMove, false);
+    pulsados.addEventListener("touchend", handleEnd, false);
 
-function handleStart(evt) {
 
-    evt.preventDefault(); //Esto evita que el navegador aplique cosas que no queremos.
 
-    var el = evt.target;
+    function handleStart(evt) {
 
-    el.style.backgroundColor = "yellow";
+        evt.preventDefault(); //Esto evita que el navegador aplique cosas que no queremos.
 
-    /*
-    var ctx = el.getContext("2d");
-    var touches = evt.changedTouches;
-  
-    for (var i = 0; i < touches.length; i++) {
-      ongoingTouches.push(touches[i]);
-      var color = colorForTouch(touches[i]);
-      ctx.fillStyle = color;
-      ctx.fillRect(touches[i].pageX - 2, touches[i].pageY - 2, 4, 4);
+        var el = evt.target;
+
+        el.style.backgroundColor = "yellow";
+
+        /*
+        var ctx = el.getContext("2d");
+        var touches = evt.changedTouches;
+      
+        for (var i = 0; i < touches.length; i++) {
+          ongoingTouches.push(touches[i]);
+          var color = colorForTouch(touches[i]);
+          ctx.fillStyle = color;
+          ctx.fillRect(touches[i].pageX - 2, touches[i].pageY - 2, 4, 4);
+        }
+        */
+
+
+
     }
-    */
 
 
+    function handleMove(evt) {
+        evt.preventDefault();
 
+        var touches = evt.changedTouches;
+
+        for (var i = 0; i < touches.length; i++) {
+
+            var touch = touches[i];
+
+        }
+
+        var x = touch.pageX;
+        var y = touch.pageY;
+
+        if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
+
+            coordinates.push({ x: x, y: y });
+
+            input.value = puntosRecorridos.map(coord => `(${coord.x}, ${coord.y})`).join(" -> ");
+        }
+    }
+
+    function handleEnd(evt) {
+        evt.preventDefault();
+
+        /*
+        var el = evt.target; 
+        el.style.backgroundColor = "blue"; 
+        */
+
+
+        //if (logEvents) log(ev.type, ev, false);
+        if (evt.targetTouches.length == 0) {
+
+            evt.target.style.background = "white";
+            evt.target.style.outline = "1px solid black";
+        }
+
+    }
 }
-
-
-function handleMove(evt) {
-    evt.preventDefault();
-
-    var touches = evt.changedTouches;
-
-    for (var i = 0; i < touches.length; i++) {
-
-        var touch = touches[i];
-
-    }
-
-    var x = touch.pageX;
-    var y = touch.pageY;
-
-    if (x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom) {
-
-        coordinates.push({ x: x, y: y });
-
-        input.value = puntosRecorridos.map(coord => `(${coord.x}, ${coord.y})`).join(" -> ");
-    }
-}
-
-function handleEnd(evt) {
-    evt.preventDefault();
-
-    /*
-    var el = evt.target; 
-    el.style.backgroundColor = "blue"; 
-    */
-
-
-    //if (logEvents) log(ev.type, ev, false);
-    if (evt.targetTouches.length == 0) {
-
-        evt.target.style.background = "white";
-        evt.target.style.outline = "1px solid black";
-    }
-
-}
-//}
