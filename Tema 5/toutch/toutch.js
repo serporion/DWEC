485ad0e98634c8a3621cae5708b0cde17dbee0e9
serporion <serporionGit@hotmail.com>
2024-11-05 20:45:49 +0100

    Toutch v1

--- Tema 5/toutch/toutch.js
+++ Tema 5/toutch/toutch.js
@@ -6,17 +6,45 @@ window.onload = () =>{
     //pulsados.addEventListener('click', (e) => console.log("se ha pulsado " + e.target.id ))
 
 
-    pulsados.addEventListener(("touchstart", handleStart, false));
-    pulsados.addEventListener(("touchmove", handleMove, false));
-    pulsados.addEventListener(("touchend", handleEnd, false));
+    pulsados.addEventListener("touchstart", handleStart, false);
+    pulsados.addEventListener("touchmove", handleMove, false);
+    pulsados.addEventListener("touchend", handleEnd, false);
 
 
 
+    function handleStart(evt) {
 
-    /*<button id="log" onclick="enableLog(event);">
-        Start/Stop event logging
-    </button>
-    <button id="clearlog" onclick="clearLog(event);">Clear the log</button>
-    */
+        evt.preventDefault(); //Esto evita que el navegador aplique cosas que no queremos.
 
+        
+        // var el = document.getElementsByTagName("div"); Me da problemas porque devuelve todos los elementos. 
+                                                          //Con target, tenemos el elemento tocado y no hace falta pedir que lo localice.
+
+        var el = evt.target; 
+
+        el.style.backgroundColor = "yellow";
+        
+        /*
+        var ctx = el.getContext("2d");
+        var touches = evt.changedTouches;
+      
+        for (var i = 0; i < touches.length; i++) {
+          ongoingTouches.push(touches[i]);
+          var color = colorForTouch(touches[i]);
+          ctx.fillStyle = color;
+          ctx.fillRect(touches[i].pageX - 2, touches[i].pageY - 2, 4, 4);
+        }
+        */
+      }
+
+
+      function handleMove(evt) {
+        // Aquí puedes agregar lógica para cuando se mueve el toque (opcional)
+        // Actualmente no estamos haciendo nada con el evento touchmove.
+    }
+
+    function handleEnd(evt) {
+        // Aquí puedes agregar lógica para cuando se termina el toque (opcional)
+        // Actualmente no estamos haciendo nada con el evento touchend.
+    }
 }
